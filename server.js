e08e943101989b8da235002d9b4246e00519f2e9
Theo Upton <Theo.Upton+Git@gmail.com>
2026-03-03 21:48:43 +0000

    add basic websocket

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const http = require('http')
+const { WebSocketServer } = require('ws')
+
 const app = express()
+const server = http.createServer(app)
+const wss = new WebSocketServer({ server })
 
 app.use(express.static('public'))
 
-app.listen(3000, () => {
+wss.on('connection', (socket) => {
+  console.log('a player connected')
+
+  socket.on('message', (data) => {
+    const message = JSON.parse(data.toString())
+  })
+
+  socket.on('close', () => {
+    console.log('a player disconnected')
+  })
+})
+
+server.listen(3000, () => {
   console.log('Server running on http://localhost:3000')
 })
