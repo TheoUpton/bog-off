9253ca3630c1ba6270359da33066bd7faaacf3db
Theo Upton <Theo.Upton+Git@gmail.com>
2026-03-28 23:54:57 +0000

    create example templates for games for client and server side

--- games/example.game.js
+++ games/example.game.js
@@ -1,16 +1,26 @@
-const Game = require("./game");
-const path = require('path')
+import {Game as BaseGame, ServerHandler as BaseHandler} from "./game.js";
+import {AbstractServerHandler as AbstractHandler} from "../public/games/example/gameAPI.js";
 
-class Example extends Game{
-    static get gameName(){return path.basename(__filename, '.game.js')};
-    /** @param {import('../lobby').Lobby} lobby */
-    constructor(lobby){
-        super(lobby)
-    }
+import { fileURLToPath } from 'url';
+import { basename } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const gameName = basename(__filename, '.game.js');
 
-    receiveMessage(player, message){
+const isDev = process.env.NODE_ENV !== 'production';
 
+export class Game extends BaseGame{
+    static get minPlayers(){return 2};
+    static get maxPlayers(){return 2};
+    static get gameName(){return gameName};
+    /**@type {Omit<import('../lobby').ServerLobby, "api"> & {api:import("../public/games/example/gameAPI.js").LobbyAPI}} */
+    lobby;
+    game_init(){
+        super.game_init();
     }
 }
 
-module.export = Example
+//const {ServerHandler: Handler} = await import(`../public/games/${gameName}/gameAPI.js`);
+export class ServerHandler extends AbstractHandler(BaseHandler){
+    constructor(game){super(game);}
+    /**@type {Game} */ game;
+}
---
+++ public/games/example/gameAPI.js
@@ -0,0 +1,81 @@
+import * as API from "../../base-gameAPI.js";
+
+export class ServerAPI extends API.ServerAPI{
+    constructor(sender, handler, broadcaster){super(sender, handler, broadcaster);}
+    get send(){
+        const sender = this.sender;
+        const superSend = super.send;
+        return {
+            ...superSend,
+            
+        };
+    }
+    get broadcast(){
+        const broadcaster = this.broadcaster.bind(this);
+        const superBroadcast = super.broadcast;
+        const client = this.client;
+        return {
+            ...superBroadcast,
+            
+        }
+    }
+    get receive(){
+        /**@type {import("../../../games/tic-tac-toe.game.js").ServerHandler} */
+        const handler = this.handler;
+        return (message) => {
+            if(super.receive(message) !== false) return true;
+            switch(message.type){
+                case handler.receive_turn.name: handler.receive_turn(message.move.row, message.move.col); break;
+                default: return false
+            }
+            return true;
+        }
+    }
+}
+export const AbstractServerHandler = (Handler) => class extends Handler{
+    /**@returns {ServerAPI} */
+    get api(){return super.api;}
+    /**@returns {import("../../../games/example.game.js").Game} */
+    get game(){return super.game;}
+}
+const serverProto = AbstractServerHandler(class{}).prototype;
+export class LobbyAPI extends API.LobbyAPI{
+    constructor(lobby, broadcaster, ack_code){super(lobby, broadcaster, ack_code);}
+    get broadcast(){
+        const broadcaster = this.broadcaster.bind(this);
+        const superBroadcast = super.broadcast;
+        return {
+            ...superBroadcast,
+
+        } 
+    }
+}
+export class ClientAPI extends API.ClientAPI{
+    constructor(sender, handler){super(sender, handler);}
+    get send() {
+        const sender = this.sender;
+        const superSend = super.send;
+        return {
+            ...superSend,
+            
+        };
+    }
+    get receive(){
+        /**@type {import("./game.js").ClientHandler} */
+        const handler = this.handler;
+        return (message) => {
+            if(super.receive(message) !== false) return true;
+            switch(message.type){
+                default: return false;
+            }
+            return true;
+        };
+    }
+}
+export const AbstractClientHandler = (Handler) => class extends Handler{
+    /**@returns {ClientAPI} */
+    get api(){return super.api;}
+    /**@returns {import("./game.js").Game}*/
+    get game(){return super.game;}
+}
+const clientProto = AbstractClientHandler(class {}).prototype;
