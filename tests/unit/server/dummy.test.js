ba6a7df11672384093d8f299e9685545115d6dfd
Theo Upton <Theo.Upton+Git@gmail.com>
2026-04-21 21:32:25 +0100

    chore: add vitest and test folder structure

---
+++ tests/unit/server/dummy.test.js
@@ -0,0 +1,8 @@
+// tests/unit/server/dummy.test.js
+import { describe, it, expect } from 'vitest';
+
+describe('setup check', () => {
+  it('works', () => {
+    expect(true).toBe(true);
+  });
+});
