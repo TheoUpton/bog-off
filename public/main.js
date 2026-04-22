c8162c27b07490a680f2958ee50699a4a1749857
Theo Upton <Theo.Upton+Git@gmail.com>
2026-04-22 23:32:52 +0100

    refactor(client): restructure main.js for clearer narrative flow (WIP)

--- public/main.js
+++ public/main.js
@@ -1,6 +1,39 @@
 import { connect } from "./connection.js";
+import { me, profileDOM } from "./me.js";
+import { home } from "./home.js";
+import { lobbyDOM } from "./lobby.js";
+import { Game } from "./base-game.js";
 
 const allPages = document.getElementsByClassName("page");
-allPages.hideAll = () => Array.from(allPages).forEach(page => page.classList.add("hide"));
+const hideAll = () => Array.from(allPages).forEach(page => page.classList.add("hide"));
 
 connect();
+showHome();
+
+function showHome(){
+    profileDOM.unlock();
+    hideAll();
+    home.show();
+    me.onJoinLobby = showLobby;
+}
+function showLobby(){
+    const lobby = lobbyDOM.lobby;
+    hideAll();
+    profileDOM.unlock();
+    lobbyDOM.show();
+    lobbyDOM.onLeave = showHome;
+    lobby.onGameSelected = animateSelection;
+    lobby.onGameLoad = showGame;
+}
+
+function animateSelection(gameStr){
+    profileDOM.lock();
+    lobbyDOM.game_select_animation();
+}
+
+/**@param {Game} game */
+function showGame(game){
+    hideAll();
+    game.show();
+    game.onEnd = showLobby;
+}
