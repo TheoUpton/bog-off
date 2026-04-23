8bc902bb28159e8fce70949b44ff7622f6b03595
Theo Upton <Theo.Upton+Git@gmail.com>
2026-04-23 01:45:59 +0100

    fix: correct import path in user tests

--- tests/unit/public/shared/user.test.js
+++ tests/unit/public/shared/user.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-import { User } from "../../../../user.js";
+import { User } from "../../../../public/shared/user.js";
 
 describe("User creation", () => {
     let user;
